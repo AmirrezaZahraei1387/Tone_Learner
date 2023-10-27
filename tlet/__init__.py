9511f6a9c775b8fd2bf5a1801bb07d2a996c1652
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-10-26 21:21:15 -0600

    an __init__ file is writren for the package tlet

---
+++ tlet/__init__.py
@@ -0,0 +1,9 @@
+from tlet.tone import ToneOPoly
+
+from tlet.oi import read_audio
+from tlet.oi import write_audio
+
+from tlet.prot import PutIn1D
+from tlet.prot import tletprocess
+from tlet.prot import tpredict_audio
+from tlet.prot import normalize_size
